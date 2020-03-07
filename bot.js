ad69fa9daca26aa9d4b0b49e20477956a489ff78
rebazluqman <58704479+rebazluqman@users.noreply.github.com>
2020-03-07 05:32:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,9 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://3rb-evets.glitch.me/`);
-}, 280000);
 
-// كل البكجات الي ممكن تحتجها في اي بوت 
-const { Client, RichEmbed } = require("discord.js");
-var { Util } = require('discord.js');
-const {TOKEN, YT_API_KEY, prefix, devs} = require('./config')
-const client = new Client({ disableEveryone: true})
-const ytdl = require("ytdl-core");
-const canvas = require("canvas");
-const Canvas = require("canvas");
-const convert = require("hh-mm-ss")
-const fetchVideoInfo = require("youtube-info");
-const botversion = require('./package.json').version;
-const simpleytapi = require('simple-youtube-api')
-const moment = require("moment");
-const fs = require('fs');
-const util = require("util")
-const gif = require("gif-search");
-const opus = require("node-opus");
-const ms = require("ms");
-const jimp = require("jimp");
-const { get } = require('snekfetch');
-const guild = require('guild');
-const dateFormat = require('dateformat');//npm i dateformat
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
-const hastebins = require('hastebin-gen');
-const getYoutubeID = require('get-youtube-id');
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const pretty = require("pretty-ms");
-client.login(TOKEN);
-const queue = new Map();
-var table = require('table').table
-const Discord = require('discord.js');
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame("t!help", "online");
-});
-
-
+ const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "-"
+ 
+ 
 
 client.on("message", async message => {
   if (message.content.startsWith(prefix + "timeon")) {
@@ -100,14 +57,10 @@ client.on("message", async message => {
 
 
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag} !`);
-  client.user.setActivity("#help ", { type: "WATCHING" });
-});
 
 
 
-//////////////////////////////////
+
 client.on("message", async message => {
   if (message.content.startsWith(prefix + "invite")) {
     let invite = new Discord.RichEmbed()
@@ -126,7 +79,7 @@ client.on("message", async message => {
 
 
 
-/////////////////
+
 
 
 client.on("message", message => {
